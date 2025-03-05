e5a8371e16bca5a63901e31caabaf0992258aa95
Burush0 <burush02@gmail.com>
2025-03-05 13:09:47 +0100

    Added some unit tests for maze class

---
+++ src/tests.py
@@ -0,0 +1,41 @@
+import unittest
+from maze import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    
+    def test_empty_maze(self):
+        num_cols = 0
+        num_rows = 0
+        m2 = Maze(0,0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m2._cells),
+            num_cols,
+        )
+    
+    def test_maze_large(self):
+        num_cols = 30
+        num_rows = 30
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+if __name__ == "__main__":
+    unittest.main()
