8fcacb9f2c58ed9cdfd336580f3911c3ba0645e0
Burush0 <burush02@gmail.com>
2025-03-05 12:24:09 +0100

    Added a cell class that represents a grid cell in a maze

--- src/main.py
+++ src/main.py
@@ -1,11 +1,30 @@
 from graphics import Window, Point, Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    point1 = Point(20, 40)
-    point2 = Point(100, 400)
-    line = Line(point1, point2)
-    win.draw_line(line, "black")
+    
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
+
+    cell2 = Cell(win)
+    cell2.has_top_wall = False
+    cell2.draw(300, 300, 500, 500)
+
+    cell3 = Cell(win)
+    cell3.has_right_wall = False
+    cell3.draw(125, 125, 225, 225)
+
+    cell4 = Cell(win)
+    cell4.has_bottom_wall = False
+    cell4.has_right_wall = False
+    cell4.draw(250, 250, 275, 275)
+
+    cell2.draw_move(cell3)
+
+    cell3.draw_move(cell4, True)
+    
     win.wait_for_close()
 
 main()
